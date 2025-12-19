b855b10c7d3ea0993bcceb807a1cc4c5caedf34b
MML0 <h1337124lat@gmail.com>
2025-12-19 12:04:17 +0330

    start intro

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -41,7 +41,12 @@ function showLegacyImage() {
   // Hide legacy image after 2 seconds
   setTimeout(() => {
     legacyImage.style.opacity = 0;
-  }, 2000); 
+    setTimeout(() => {
+      const story = document.querySelector('.story');
+      story.style.display = 'none'; // Show the story section
+     }, 1000); 
+  
+  }, 3000); 
 }
 function showPersonImages() {
   const personImages = document.querySelectorAll('.person-img');
@@ -51,7 +56,7 @@ function showPersonImages() {
     setTimeout(() => {
       img.style.opacity = 1;
     //   img.classList.add('animate-wipe');  
-    }, 1000 * (index + 1));  // Delay for each person
+    }, 1000 * (index + 2));  // Delay for each person
   });
 
   // After all images are shown, wipe person images one by one
@@ -61,28 +66,30 @@ function showPersonImages() {
         img.style.opacity = 0; // Fade out images
       }, 1000 * (index + 1));
     });
-  }, 7000000);  // Delay the wipe to let all images show
+  }, 700000);  // Delay the wipe to let all images show
 }
-
-setTimeout(Loader.show, 100); // Show the loader after 1 second
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Show loader, then hide it after 3 seconds
-  setTimeout(Loader.hide, 500); // Hide the loader after 4 seconds
-
-  // After the loader hides, show the story section with animations
-  setTimeout(showStorySection, 1500);  // Adjust to fit loader hide time
-
 function showStorySection() {
   const story = document.querySelector('.story');
   story.style.display = 'flex'; // Show the story section
   
-  // Call the functions to handle legacy image and person images
   showLegacyImage();
-  showPersonImages();
+  // Call the functions to handle legacy image and person images
+//   setTimeout(() => {
+//      showPersonImages();
+//    }, 500); 
 }
+
+
+
+
+setTimeout(Loader.show, 100); // Show the loader after 1 second
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Show loader, then hide it after 3 seconds
+  setTimeout(Loader.hide, 2500); // Hide the loader after 4 seconds
+
+  // After the loader hides, show the story section with animations
+  setTimeout(showStorySection, 4500);  // Adjust to fit loader hide time
+
 
 });
